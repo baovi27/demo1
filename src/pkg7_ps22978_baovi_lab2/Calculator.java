3a26c0decbf4a3112adb585d3ab3c4d3b276e264
baovi27 <99228432+baovi27@users.noreply.github.com>
2022-08-26 19:46:36 +0700

    1

---
+++ src/pkg7_ps22978_baovi_lab2/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg7_ps22978_baovi_lab2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Calculator 
+{
+    
+    
+}
---
+++ src/pkg7_ps22978_baovi_lab2/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg7_ps22978_baovi_lab2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        QLSVform frLSVform= new QLSVform();
+        frLSVform.setVisible(true);
+              
+    }
+    
+}
